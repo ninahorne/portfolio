421babc2ce4955205239d9fccd1e404c655bbc87
ninahorne <nina.horne1221@gmail.com>
2021-08-29 12:42:50 -0400

    Simplified the Parallax

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -5,28 +5,22 @@ function generate60Stars() {
   
   let shootingStarOne = document.createElement('div');
  
-
   shootingStarOne.classList.add('shooting-star-1');
 
-
   stars.appendChild(shootingStarOne);
-
-
-
-
 
   stars.setAttribute('style', `height: ${bodyHeight}px`)
   document.body.appendChild(stars);
   for (let i = 0; i < 1000; i++) {
     let star = document.createElement("div");
-    star.classList.add("star", 'rellax');
+    star.classList.add("star");
     const top = Math.random()*bodyHeight + 'px';
     const left = Math.random()*100 + 'vw';
     star.setAttribute('style',`top:${top};left: ${left}`);
     if(i%5 === 0){
         const randomNumber = Math.round(Math.random()*10);
         const direction = randomNumber % 2 == 0 ? -1 : 1;
-        star.setAttribute('data-rellax-speed', direction * Math.round(Math.random()*5));
+        // star.setAttribute('data-rellax-speed', direction * Math.round(Math.random()*5));
     }
    
     const stars = document.querySelector(".stars");
@@ -35,34 +29,9 @@ function generate60Stars() {
   }
 }
 
-function moonEffects() {
-  var moon = document.querySelector(".moon");
-
-  moon.addEventListener("mousemove", function (t) {
-    var e = -(window.screen.width / 2 - t.pageX) / 30,
-      n = (window.screen.height / 2 - t.pageY) / 10;
-    moon.setAttribute(
-      "style",
-      "transform: rotateY(" +
-        e +
-        "deg) rotateX(" +
-        n +
-        "deg);-webkit-transform: rotateY(" +
-        e +
-        "deg) rotateX(" +
-        n +
-        "deg);-moz-transform: rotateY(" +
-        e +
-        "deg) rotateX(" +
-        n +
-        "deg)"
-    );
-  });
-}
 
 setTimeout(() => {
   generate60Stars();
   var rellax = new Rellax('.rellax');
 
-//   moonEffects();
 }, 100);
